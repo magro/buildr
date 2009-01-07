364a90dafc619f20fd11e2c0a5550b7a6a340858
Daniel Spiewak <djspiewak@gmail.com>
2009-01-06 20:58:52 -0600

    Working compiler

--- lib/buildr/clojure/compiler.rb
+++ lib/buildr/clojure/compiler.rb
@@ -3,6 +3,8 @@ require 'buildr/core/common'
 require 'buildr/core/compile'
 require 'buildr/packaging'
 
+require 'buildr/java/commands'
+
 module Buildr
   module Clojure
     class Cljc < Buildr::Compiler::Base
@@ -10,34 +12,50 @@ module Buildr
         def clojure_home
           @home ||= ENV['CLOJURE_HOME']
         end
-    
-        def dependencies
-          File.expand_path('clojure.jar', clojure_home)
-        end
       end
       
       OPTIONS = [:libs]
       
-      Java.classpath << dependencies
-      
       specify :language => :clojure, :sources => :clojure, :source_ext => :clj,
-              :target => 'classes', :target_ext => 'class', :packaging => :jar
+              :target => 'classes', :target_ext => 'clj', :packaging => :jar
       
       def initialize(project, options)
         super
         
         options[:libs] ||= []
-        
-        Java.classpath << project.path_to(:target, :clojure)
-        Java.classpath << project.path_to(:source, :main, :clojure)
       end
       
       def compile(sources, target, dependencies) #:nodoc:
         check_options options, OPTIONS
         
-        Java.load
-        Java.java.lang.System.setProperty('clojure.compile.path', File.expand_path(target))
-        Java.clojure.lang.Compile.main(options[:libs].to_java(Java.java.lang.String))
+        source_paths = sources.select { |source| File.directory?(source) }
+        cp = dependencies + source_paths + [
+          File.expand_path('clojure.jar', Cljc.clojure_home)
+        ]
+        cp_str = inner_classpath_from(cp).join(File::PATH_SEPARATOR) + 
+                 File::PATH_SEPARATOR + File.expand_path(target)
+        
+        cmd_args = [
+          '-classpath', "'#{cp_str}'",
+          "-Dclojure.compile.path='#{File.expand_path(target)}'",
+          'clojure.lang.Compile'
+        ] + options[:libs]
+        
+        trace "Target: #{target}"
+        trace "Sources: [ #{sources.join ', '} ]"
+        
+        cmd = 'java ' + cmd_args.join(' ')
+        trace cmd
+        system cmd
+      end
+      
+    private
+    
+      def inner_classpath_from(cp)
+        Buildr.artifacts(cp.map(&:to_s)).map do |t| 
+          task(t).invoke
+          File.expand_path t
+        end
       end
     end
   end

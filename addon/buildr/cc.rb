4e211f8cc18d2ff7b771d8cf253f16412e7c00f9
Daniel Spiewak <djspiewak@apache.org>
2009-07-13 22:20:55 -0500

    Added prototype continuous compilation task

---
+++ addon/buildr/cc.rb
@@ -0,0 +1,51 @@
+module Buildr
+  module CC
+    include Extension
+    
+    class << self
+      def check_mtime(dirs, ext, old_times)
+        times = old_times
+        changed = []
+        
+        dirs.each do |dir|
+          Dir.glob "#{dir}/**/*.{#{ext.join ','}}" do |fname|
+            if old_times[fname].nil? || old_times[fname] < File.mtime(fname)
+              times[fname] = File.mtime fname
+              changed << fname
+            end
+          end
+        end
+        
+        [times, changed]
+      end
+    end
+    
+    first_time do
+      Project.local_task :cc
+    end
+    
+    before_define do |project|
+      project.task :cc => :compile do
+        dirs = project.compile.sources.map(&:to_s)
+        ext = [:scala].map(&:to_s)               # TODO
+        times, _ = Buildr::CC.check_mtime dirs, ext, {}     # establish baseline
+        
+        info "Monitoring directories: [#{dirs.join ', '}]"
+        
+        while true
+          sleep 0.2
+          
+          times, changed = Buildr::CC.check_mtime dirs, ext, times
+          unless changed.empty?
+            info "Detected changes: [#{changed.join ', '}]"
+            project.task(:compile).invoke
+          end
+        end
+      end
+    end
+  end
+  
+  class Project
+    include CC
+  end
+end
